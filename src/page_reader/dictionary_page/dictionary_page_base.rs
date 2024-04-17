4ebab5d49ce01d3cd21d02c569d92a9deb113529
Shengxuan Liu <shengxuan.liu@bytedance.com>
2024-04-16 17:03:51 -0700

    Update the Dictionary Page Readers for Int32/64 and Float32/64
    This commit removes the generic of Dictionary Page Reader and introduced
    Dictionary Page Readers for Int32/64 and Float32/64 and supporting filters

--- src/page_reader/dictionary_page/dictionary_page_base.rs
+++ src/page_reader/dictionary_page/dictionary_page_base.rs
@@ -13,6 +13,32 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub trait DictionaryPageNew {
+    fn validate(&self, index: usize) -> bool;
+
+    fn find_int32(&self, _index: usize) -> i32 {
+        i32::default()
+    }
+
+    fn find_float32(&self, _index: usize) -> f32 {
+        f32::default()
+    }
+
+    fn find_int64(&self, _index: usize) -> i64 {
+        i64::default()
+    }
+
+    fn find_float64(&self, _index: usize) -> f64 {
+        f64::default()
+    }
+
+    fn get_num_values(&self) -> usize;
+
+    fn get_type_size(&self) -> usize;
+
+    fn is_zero_copied(&self) -> bool;
+}
+
 pub trait DictionaryPage<T> {
     fn validate(&self, index: usize) -> bool;
 
